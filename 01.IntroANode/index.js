61a95fda6b043ca61fad207b02b398455de24892
zlopez <zlopezleg@gmail.com>
2022-06-02 22:51:21 -0500

    aprendiendo a cargar paquetes con npm

---
+++ 01.IntroANode/index.js
@@ -0,0 +1,8 @@
+var colors = require ('colors') //requerimiento de la libreria colors
+
+console.log("HOLA MUNDO DESDE LA CONSOLA".magenta)
+
+for(let i=1;i<=10;i++){
+	console.log("El resultado es: ".yellow + i*2)
+}
+
